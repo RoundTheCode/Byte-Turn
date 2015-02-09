f65e9859f3caf8982f81b02891f5956237a97b75
Dave <davidgrace00@gmail.com>
2015-02-09 20:06:00 +0000

    Added test results.

---
+++ RoundTheCode.ByteTurn.Test/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoundTheCode.ByteTurn.Services;
+using RoundTheCode.ByteTurn.Data.Listing;
+using System.IO;
+
+namespace RoundTheCode.ByteTurn.Test
+{
+    [TestClass]
+    public partial class ListingInformation
+    {
+        public string path = @"..\..\..\RoundTheCode.ByteTurn.Web\Test";
+        public string parentPath = @"..\..\..\RoundTheCode.ByteTurn.Web";
+
+
+        public ListingInformation()
+        {
+            path = Path.GetFullPath(path);
+            parentPath = Path.GetFullPath(parentPath);
+
+            if (!ListingService.Exists(path))
+            {
+                ListingService.Create("Test", parentPath, Data.Listing.ListingTypeOption.Directory, Data.Listing.DuplicateListingActionOption.NoAction);
+            }
+
+            if (ListingService.Exists(path + "@\test.txt"))
+            {
+                ListingService.Delete(path + "@\test.txt");
+            }
+
+            ListingService.Create("test.txt", path, ListingTypeOption.File, DuplicateListingActionOption.NoAction);
+        }
+
+        [TestMethod]
+        public void DirectoryInfo()
+        {
+            var d = new DirectoryData(path);
+
+            Assert.AreEqual(d.Directory, parentPath);
+            Assert.AreEqual(d.FullFilePath, path);
+            Assert.AreEqual<ListingTypeOption>(d.ListingType, ListingTypeOption.Directory);
+            Assert.AreEqual(d.Name, "Test");        
+        }
+
+        [TestMethod]
+        public void FileInfo()
+        {
+            var f = new FileData(path + @"\test.txt");
+
+            Assert.AreEqual(f.Directory, path);
+            Assert.AreEqual(f.Extension, ".txt");
+            Assert.AreEqual(f.FullFilePath, path + @"\test.txt");
+            Assert.AreEqual<ListingTypeOption>(f.ListingType, ListingTypeOption.File);
+            Assert.AreEqual(f.Name, "test.txt");
+            Assert.AreEqual(f.Size.Bytes, 0);
+            Assert.AreEqual(f.Size.Gigabytes, 0);
+            Assert.AreEqual(f.Size.Kilobytes, 0);
+            Assert.AreEqual(f.Size.Megabytes, 0);
+            Assert.AreEqual(f.Size.Terabytes, 0);
+        }
+    }
+}
--- RoundTheCode.ByteTurn.Web/Controllers/HomeController.cs
+++ RoundTheCode.ByteTurn.Web/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace RoundTheCode.ByteTurn.Web.Controllers
             allowedFileExtensions.Add("gif");
             allowedFileExtensions.Add("png");
 
-            var path = ListingService.WebUpload(HttpContext, FileUpload, @"C:\Users\david\2\4", allowedFileExtensions, Data.Listing.DuplicateListingActionOption.AppendNumber);
+            var path = ListingService.WebUpload(HttpContext, FileUpload, Server.MapPath(@"Upload\2\4"), allowedFileExtensions, Data.Listing.DuplicateListingActionOption.AppendNumber);
 
             return View();
         }
